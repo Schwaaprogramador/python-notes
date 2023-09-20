85ec356c4c9b1cdba3bd99015e7c9a8468fa671b
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-09-20 13:48:57 -0500

    archivo inicial

---
+++ Mi_Documentacion.py
@@ -0,0 +1,3 @@
+#Con hasttag se hacen los comentarios.
+print('hola') # simple , para ver algo por consola. Equivalente = console.log
+# En python no es necesario poner puntos y comas al final de cada linea.
