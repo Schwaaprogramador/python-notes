886386cded5ca3b91ae916fef44e2662c379fe29
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-09-20 19:42:19 -0500

    notas iniciales

---
+++ 01_variables.py
@@ -0,0 +1,13 @@
+#Las variables deben ser escritas todas en minuscula.
+#Se recomienda usar el SNAKE CASE.
+
+my_string_variable = 'Variable escrita con snake case.'
+myStringVariable = 'Variable escrita con camelCase. '
+print(my_string_variable, myStringVariable) # Print puede recibir varios argumentos. Van separados por coma.
+
+
+#Mini sistema de inputs para crear variables
+firts_name = input('Cual es tu nombre')
+age_input = input('Cual es tu edad')
+#Esto se realiza con la funcion nativa input
+print(firts_name, age_input)
--- Mi_Documentacion.py
+++ Mi_Documentacion.py
@@ -1,3 +1,16 @@
 #Con hasttag se hacen los comentarios.
 print('hola') # simple , para ver algo por consola. Equivalente = console.log
 # En python no es necesario poner puntos y comas al final de cada linea.
+
+
+""" 
+Con tres comillas 
+se pueden hacer comentarios de 
+varias lineas sin tener que poner un hashtag para cada.
+Tambien se pueden utilizar las comillas simples.
+"""
+
+#La funcion type sirve para determinar el tipo de dato. Esta es nativa de python.
+print(type('hola'))
+#El resultado del anterior: <class 'str'> tipo de dato sting.
+print(type(5))
